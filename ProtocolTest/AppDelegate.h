ba3fac6b8f0693c4c16f50a262fc6d27f5b11a78
Eddie <Eddie@galchenko.local>
2018-04-07 14:10:44 +0300

    Initial Commit

---
+++ ProtocolTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProtocolTest
+//
+//  Created by Eddie on 07.04.2018.
+//  Copyright © 2018 Eddie Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProtocolTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProtocolTest
+//
+//  Created by Eddie on 07.04.2018.
+//  Copyright © 2018 Eddie Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
